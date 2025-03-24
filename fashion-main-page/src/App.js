5b6410f67927a6ec3c5e571ca9b6c170b600554f
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2025-03-24 19:14:06 +0400

    arrivals and brands section

--- fashion-main-page/src/App.js
+++ fashion-main-page/src/App.js
@@ -1,3 +1,5 @@
+import Arrivals from "./components/arrivals/Arrivals.jsx";
+import Brands from "./components/brands/Brands.jsx";
 import Header from "./components/header/Header.jsx";
 import Promo from "./components/promo/Promo.jsx";
 
@@ -7,6 +9,8 @@ function App() {
     <div className="App">
       <Header />
       <Promo />
+      <Brands />
+      <Arrivals />
     </div>
   );
 }
