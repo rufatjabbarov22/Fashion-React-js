6c4600fc8a16feb8ddbed869d36e7748acc3cb1e
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2025-03-24 18:59:37 +0400

    header and promo section

---
+++ fashion-main-page/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/header/Header.jsx";
+import Promo from "./components/promo/Promo.jsx";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Promo />
+    </div>
+  );
+}
+
+export default App;
+ 
